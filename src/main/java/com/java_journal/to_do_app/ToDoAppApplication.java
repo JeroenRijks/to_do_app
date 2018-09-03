3638ccbee7a4a0e5dda108a91c4489ea8a348dec
Jeroen <jeroen-rijks@hotmail.com>
2018-09-03 15:25:47 +0100

    first commit - attach all relevant files

---
+++ src/main/java/com/java_journal/to_do_app/ToDoAppApplication.java
@@ -0,0 +1,12 @@
+package com.java_journal.to_do_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+}
